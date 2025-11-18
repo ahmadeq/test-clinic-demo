62d9ccfb17f5c23d33dd7a299d2910dd628488cd
Ahmad Iqtaish <ahmad.iqtaish@joacademy.com>
2025-11-19 00:47:16 +0300

    AAAAAAAAA

--- components/services/supabase.ts
+++ components/services/supabase.ts
@@ -1,12 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error(
-    "Missing Supabase URL or Key. Please check your environment variables."
-  );
-}
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error(
+//     "Missing Supabase URL or Key. Please check your environment variables."
+//   );
+// }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// export const supabase = createClient(supabaseUrl, supabaseKey);
